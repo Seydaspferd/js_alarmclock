a14d39623c7a18d00845e19854d978e48c1b9dbf
Seydaspferd <damethe@hotmail.com>
2020-05-05 19:34:11 +0200

    finished alarm clock and added timer, also added images and sounds

--- main.js
+++ main.js
@@ -25,15 +25,17 @@ function timeDisplay() {
 function getInput() {
   inputTime = document.getElementById('timeselect').value;
 
-  document.getElementById('test').innerHTML = inputTime;
-
-  //reset alarm message when input gets changed to future time
+  //reset alarm message when input gets changed to future time and stop alarm sound and remove clockimg
   document.getElementById('alarm').innerHTML = '';
+  document.getElementById('clockimg1').style.display = 'none';
+
+  document.getElementById('audiotag1').pause();
+  document.getElementById('audiotag1').currentTime = 0;
 }
 
 //display current time every second
 //get input time every second
-//compare input time and real time every second
+//compare input time and current time every second
 
 function init() {
   setInterval(timeDisplay, 1000);
@@ -46,5 +48,57 @@ function init() {
 function timeCompare() {
   if (currentTime === inputTime) {
     document.getElementById('alarm').innerHTML = 'WAKE UP!';
+
+    document.getElementById('clockimg1').style.display = 'block';
+
+    document.getElementById('audiotag1').play();
+  }
+}
+
+// Timer JS
+
+document.getElementById('startbtn').addEventListener('click', timerOuter);
+
+var intervalVar;
+var inputMin;
+var inputSec;
+
+function timerOuter() {
+  inputMin = document.getElementById('minuteinput').value;
+  inputSec = document.getElementById('secondinput').value;
+
+  document.getElementById('startbtn').removeEventListener('click', timerOuter);
+  document.getElementById('stopbtn').addEventListener('click', stopFunction);
+
+  intervalVar = setInterval(timerFunction, 1000);
+
+  function timerFunction() {
+    if (inputSec > 0) {
+      inputSec--;
+      document.getElementById('secondinput').value = inputSec;
+    } else if (inputMin > 0 && inputSec === 0) {
+      inputMin--;
+      document.getElementById('minuteinput').value = inputMin;
+
+      document.getElementById('secondinput').value = 59;
+      inputSec = document.getElementById('secondinput').value;
+    } else {
+      document.getElementById('inputalarm').innerHTML = 'Times Up!';
+      document.getElementById('secondinput').value = 0;
+      document.getElementById('minuteinput').value = 0;
+      clearInterval(intervalVar);
+      document.getElementById('startbtn').addEventListener('click', timerOuter);
+      document.getElementById('clockimg2').style.display = 'block';
+      document.getElementById('audiotag2').play();
+    }
+  }
+
+  function stopFunction() {
+    clearInterval(intervalVar);
+    document.getElementById('startbtn').addEventListener('click', timerOuter);
+    document.getElementById('clockimg2').style.display = 'none';
+    document.getElementById('inputalarm').innerHTML = '';
+    document.getElementById('audiotag2').pause();
+    document.getElementById('audiotag2').currentTime = 0;
   }
 }
